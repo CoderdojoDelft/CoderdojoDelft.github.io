8296dc4aed18e41492933299b76f3000e3463ad1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-23 15:21:03 +0000

    Deploy CoderdojoDelft/CoderdojoDelft.github.io to github.com/CoderdojoDelft/CoderdojoDelft.github.io.git:master

---
+++ precache-manifest.9ed7f7c605505ac182e5ffb2ab843d74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4e78b2af8e6a36103786bf9043d26a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a45ae2b5ca5a0f03a259",
+    "url": "/static/js/2.6a827f94.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.6a827f94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbb1634dc66f68b26cd3",
+    "url": "/static/js/main.6cde92ab.chunk.js"
+  },
+  {
+    "revision": "6aee7cb69c97156a943e",
+    "url": "/static/js/runtime-main.a2cf05f3.js"
+  }
+]);
