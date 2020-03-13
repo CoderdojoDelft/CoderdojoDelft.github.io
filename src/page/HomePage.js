3168b1fd6b1928b82d426d306386d44d97a0ac0f
Erna Oudman <ernaoudman@hotmail.com>
2020-03-13 17:02:15 +0100

    react-scripts updats / Corona mededeling

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -10,6 +10,11 @@ import { routes } from '../routes';
 export class HomePage extends Component {
     render() {
         let upcoming;
+        let warning = {
+            color: '#b30000',
+            'font-weight': '800',
+            'font-size': '16px'
+        }
 
         if (nextEdition) {
             upcoming = (
@@ -30,6 +35,8 @@ export class HomePage extends Component {
                     <p>Bij CoderDojo Delft leren kinderen programmeren, websites te bouwen, apps te ontwikkelen, programma’s te maken, games te maken en meer! We komen regelmatig bij elkaar om te leren programmeren, maar ook om andere deelnemers te ontmoeten. De CoderDojo is gratis bij te wonen omdat het volledig georganiseerd en begeleid wordt door vrijwilligers (mentoren).</p>
                     <p>Een belangrijke eigenschap van een CoderDojo is dat het plaatsvindt in een openbare ruimte en voor iedereen toegankelijk is. <a href="https://opendelft.info/">OPEN</a> (voormalig Bibliotheek DOK Delft) is daarom de perfecte plek voor onze bijeenkomsten.</p>
                     <h3>Activiteiten</h3>
+                    <p style={warning}>In verband met het Coronavirus en de daarvoor getroffen maatregelen, zal de op 28 maart geplande CoderDojo helaas niet doorgaan.</p>
+                    <p>Kijk op onze nieuwe pagina <Link to={routes.material.url}>'Aan de slag'</Link> voor leuke opdrachten om thuis mee aan de slag te gaan.</p>
                     {upcoming}
                     <p>Voor een overzicht van de komende CoderDojo's, kijk <Link to={routes.agenda.url}>hier</Link>.</p>
                     <h3>Andere locaties</h3>
