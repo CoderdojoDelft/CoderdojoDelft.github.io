8bd5605d74b5bfecc47c36ed56723117032a2f40
Erna Oudman <ernaoudman@hotmail.com>
2020-03-25 21:26:51 +0100

    1e info over online dojo's

--- src/page/RemotePage.js
+++ src/page/RemotePage.js
@@ -10,7 +10,11 @@ export class RemotePage extends Component {
             <Page>
                 <PageTitle>CoderDojo op afstand</PageTitle>
                 <PageBody>
-                    Hier komt de tekst
+                <p>In verband met het Corona-virus kunnen we tot 1 juni geen normale dojo aanbieden. Maar dat betekent niet dat jullie ons de komende maanden niet helemaal te missen. We zijn heel druk bezig met het opzetten van onze eerste online dojo.</p>
+
+                <p>Op 28 maart draaien we een kleine pilot, zodat we zelf ook kunnen uitzoeken hoe dit werkt. De voor april en mei geplande CoderDojo's zullen online worden aangeboden.</p>
+
+                <p>Na de pilot zullen we hier informatie toevoegen over de online dojo's.</p>
                 </PageBody>
             </Page>
         );
